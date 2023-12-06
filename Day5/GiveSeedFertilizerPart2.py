6f962bdd710fb99ef229fd37a636d5ac31314cc5
Krzysztof Sendera <krzys3n@gmail.com>
2023-12-06 19:40:33 +0100

    Day 5

--- Day5/GiveSeedFertilizerPart2.py
+++ Day5/GiveSeedFertilizerPart2.py
@@ -45,7 +45,7 @@ for seed_ranges in range(0, len(seeds) - 1, 2):
             location_min = location
             minimum_location_seed_range = seeds[seed_ranges] + " " + seeds[seed_ranges + 1]
 
-    for seed in range(minimum_location_value_seed-5000, minimum_location_value_seed+5000, 1):
+    for seed in range(minimum_location_value_seed-10000, minimum_location_value_seed+10000, 1):
         location = seed
         for key, valuesall in data.items():
             for values in valuesall:
